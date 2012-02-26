23b6f3fccaa7ec755a75f26fae43befc6a9b2792
Micheal19840929 <Micheal19840929@81659778-87ce-11de-a4f2-b7219ab9ea91>
2012-02-26 08:55:15 +0000

    初始导入。

---
+++ trunk/Yon/Yon01/include/IReferencable.h
@@ -0,0 +1,13 @@
+#ifndef _YON_CORE_IREFERENCABLE_H_
+#define _YON_CORE_IREFERENCABLE_H_
+
+namespace yon{
+
+	namespace core{
+		class IReferencable{
+
+		};
+	}
+}
+
+#endif
---
+++ trunk/Yon/Yon01/include/yon.h
@@ -0,0 +1,15 @@
+#ifndef _YON_YON_H_
+#define _YON_YON_H_
+
+#include "config.h"
+
+#include "EVideoType.h"
+#include "YonEngine.h"
+#include "YonEngineParameters.h"
+
+namespace yon{
+
+	extern "C" YON_API YonEngine* CreateEngine(const SYonEngineParameters& param);
+}
+
+#endif
